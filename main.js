19ba896ef70a5b61f60b6815d92b86026509d2aa
Ålvee <120122005+Glitch-vee@users.noreply.github.com>
2024-06-22 18:45:39 +0600

    Update main.js

--- main.js
+++ main.js
@@ -18,15 +18,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Add shapes
     addShape('shape-1', 300, 300, 15, 25);
-    addShape('shape-2', 500, 500, 50, 70);
-    addShape('shape-3', 200, 200, 70, 20);
+    addShape('shape-2', 400, 500, 50, 70);
+    addShape('shape-3', 200, 200, 45, 20);
     addShape('shape-4', 400, 400, 20, 60);
-    addShape('shape-5', 150, 150, 80, 40);
+    addShape('shape-5', 150, 150, 30, 40);
     addShape('shape-6', 250, 250, 40, 50);
     addShape('shape-7', 350, 350, 30, 75);
-    addShape('hollow', 200, 200, 10, 10);
+    addShape('hollow', 200, 200, 70, 10);
     addShape('hollow', 300, 300, 60, 20);
     addShape('small', 100, 100, 50, 10);
-    addShape('small', 100, 100, 80, 90);
-    addShape('small', 100, 100, 20, 80);
+    addShape('small', 100, 100, 60, 90);
+    addShape('small', 100, 100, 30, 80);
 });
